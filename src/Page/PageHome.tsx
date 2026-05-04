d7aa8960a155364226e9536c647dadfe9696518d
Sergino <serginorakotomalala@gmail.com>
2026-05-04 10:56:19 +0300

    marge 13

--- src/Page/PageHome.tsx
+++ src/Page/PageHome.tsx
@@ -37,7 +37,7 @@ function PageHome() {
   };
 
   return (
-    <div className="min-h-screen  from-gray-900 via-gray-800 to-gray-900 flex items-center justify-center py-8 px-4 mt-16">
+    <div className="min-h-screen  from-gray-900 via-gray-800 to-gray-900 flex items-center justify-center py-8 px-4 mt-13">
       <div className="max-w-7xl w-full mx-auto">
         <div className="flex flex-col lg:flex-row items-center justify-between gap-12">
           
